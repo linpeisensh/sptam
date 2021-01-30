3f3d5099d6a653f590e493523040f04cc61793cb
Peisen <linpeisensh@gmail.com>
2021-01-30 22:45:40 +0100

    comp3

--- dynaseg.py
+++ dynaseg.py
@@ -215,6 +215,71 @@ class DynaSeg():
         return c
 
 
+    # def dyn_seg_rec(self, frame, iml):
+    #     '''
+    #     dynamic segmentation based on projection error and object recording
+    #     :param frame: original sptam frame after tracking
+    #     :param iml: left image
+    #     :return:
+    #     c: dynamic segmentation of iml
+    #     '''
+    #     frame_gray = cv.cvtColor(iml, cv.COLOR_BGR2GRAY)
+    #     error, imgpts, P = self.projection(frame, frame_gray)
+    #
+    #     merror = np.array(error)
+    #     for i in range(len(error)):
+    #         if imgpts[i][0] < 400:
+    #             merror[i] = max(merror[i] - 15 * 15, 0)
+    #         if imgpts[i][0] > 900:
+    #             merror[i] = max(merror[i] - 325, 0)
+    #     ge = merror > np.median(error)
+    #
+    #     image = iml.astype(np.uint8)
+    #     prediction = self.coco.compute_prediction(image)
+    #     top = self.coco.select_top_predictions(prediction)
+    #     masks = top.get_field("mask").numpy()
+    #
+    #     nobj = len(self.obj)
+    #     for i in range(nobj):
+    #         cm = np.where(self.obj[i][0] == True)
+    #         cmps = np.array(list(zip(cm[1], cm[0]))).astype(np.float32)
+    #         nmps, st, err = cv.calcOpticalFlowPyrLK(self.old_gray, frame_gray, cmps, None, **self.lk_params)
+    #         nm = np.zeros_like(self.obj[i][0]).astype(np.bool)
+    #         for nmp in nmps:
+    #             x, y = round(nmp[1]), round(nmp[0])
+    #             if 0 <= x < self.h and 0 <= y < self.w:
+    #                 nm[x, y] = True
+    #         self.obj[i][0] = nm
+    #
+    #
+    #     res = [False] * nobj
+    #     c = np.zeros((self.h, self.w))
+    #     n = len(masks)
+    #     for i in range(n):
+    #         mask = masks[i].squeeze()
+    #         ci = self.track_obj(mask)
+    #         if ci < nobj:
+    #             res[ci] = True
+    #         else:
+    #             res += [True]
+    #             nobj += 1
+    #         mask = mask.astype(np.float64)
+    #         mask_dil = cv.dilate(mask, self.kernel)
+    #         ao = 0
+    #         co = 0
+    #         for i in range(len(error)):
+    #             x, y = round(P[i][1]), round(P[i][0])
+    #             if 0 <= x < self.h and 0 <= y < self.w and mask_dil[x, y]:
+    #                 ao += 1
+    #                 if ge[i]:
+    #                     co += 1
+    #         if ao > 1 and co / ao > 0.5:
+    #             self.obj[ci][2] += 1
+    #     for obj in self.obj:
+    #         if obj[2] / obj[1]  >= self.dyn_thd or obj[2]>3:
+    #             c[obj[0]] = 255
+    #     self.old_gray = frame_gray.copy()
+    #     return c
     def dyn_seg_rec(self, frame, iml):
         '''
         dynamic segmentation based on projection error and object recording
@@ -240,6 +305,7 @@ class DynaSeg():
         masks = top.get_field("mask").numpy()
 
         nobj = len(self.obj)
+        res = [True] * nobj
         for i in range(nobj):
             cm = np.where(self.obj[i][0] == True)
             cmps = np.array(list(zip(cm[1], cm[0]))).astype(np.float32)
@@ -250,21 +316,18 @@ class DynaSeg():
                 if 0 <= x < self.h and 0 <= y < self.w:
                     nm[x, y] = True
             self.obj[i][0] = nm
-
-
-        res = [False] * nobj
+            if np.sum(nm) < 225:
+                res[i] = False
+        self.obj = np.array(self.obj, dtype=object)
+        self.obj = list(self.obj[res])
         c = np.zeros((self.h, self.w))
         n = len(masks)
         for i in range(n):
             mask = masks[i].squeeze()
             ci = self.track_obj(mask)
-            if ci < nobj:
-                res[ci] = True
-            else:
-                res += [True]
-                nobj += 1
             mask = mask.astype(np.float64)
             mask_dil = cv.dilate(mask, self.kernel)
+            self.obj[ci][0] = mask_dil.astype(np.bool)
             ao = 0
             co = 0
             for i in range(len(error)):
@@ -273,13 +336,15 @@ class DynaSeg():
                     ao += 1
                     if ge[i]:
                         co += 1
-            if ao > 1 and co / ao > 0.5:
-                self.obj[ci][2] += 1
+            if ao > 1:
+                if co / ao > 0.5:
+                    self.obj[ci][2] += 1
         for obj in self.obj:
-            if obj[2] / obj[1]  >= self.dyn_thd or obj[2]>3:
+            if obj[2] / obj[1] >= self.dyn_thd or obj[2] > 3:
                 c[obj[0]] = 255
         self.old_gray = frame_gray.copy()
-        return cv.erode(c,self.e_kernel)
+
+        return cv.erode(c, self.e_kernel)
 
 def Rt_to_tran(tfm):
   res = np.zeros((4,4))

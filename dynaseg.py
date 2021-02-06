1a22bf30cb5f6e703d7378f28f02be8f49c11f0f
Peisen <linpeisensh@gmail.com>
2021-02-06 22:56:21 +0100

    dyn0.1

--- dynaseg.py
+++ dynaseg.py
@@ -21,7 +21,7 @@ class DynaSeg():
         self.e_kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5))
 
         self.obj = np.array([])
-        self.IOU_thd = 0.2
+        self.IOU_thd = 0.1
         self.dyn_thd = 0.8
 
 

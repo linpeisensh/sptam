662ed220f6c85d891546aa59acd9709938999010
Peisen <linpeisensh@gmail.com>
2021-02-07 23:38:59 +0100

    dis

--- dynaseg.py
+++ dynaseg.py
@@ -57,7 +57,7 @@ class DynaSeg():
         disp = self.stereoMatchSGBM(iml_, imr_)
         dis = np.load(self.disp_path + str(i).zfill(6) + '.npy')
         disp[disp == 0] = dis[disp == 0]
-        points = cv.reprojectImageTo3D(disp, self.Q)
+        points = cv.reprojectImageTo3D(dis, self.Q)
         return points
 
     def track_obj(self, mask, idx):
@@ -107,7 +107,7 @@ class DynaSeg():
             cverror = cv.norm(P, imgpts, cv.NORM_L2) / len(imgpts)
         else:
             cverror = float('inf')
-        # print(cverror)
+        print(cverror)
         self.p1 = p1
         return error, imgpts, P
 
